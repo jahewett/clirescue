c63e9adcd9671c27fbeffbc1bdd158a496533784
James Hewett <jhewett@sonovate.com>
2019-05-27 16:50:26 +0100

    Updated git user name

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/jahewett/clirescue/cmdutil"
+	"github.com/jahewett/clirescue/user"
 )
 
 var (
